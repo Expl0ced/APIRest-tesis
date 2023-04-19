7a0b3ac19d4ee8ee4eb2b5a636a94c508eeafc2f
Expl0ced <56272035+Expl0ced@users.noreply.github.com>
2023-04-19 17:21:00 -0400

    UPDATE SUBIDA.JS

--- src/routes/subidas.js
+++ src/routes/subidas.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const router = express.Router();
 
@@ -26,33 +25,4 @@ router.get('/cuenta/:id', (req, res) => {
         }
     });  
 });
-=======
-const express = require('express');
-const router = express.Router();
-
-const mysqlConnection  = require('../database.js');
-
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    mysqlConnection.query('select id, nombre, imagen, fecha_creacion, idUser from files where idUser=?',[ id ], (err, rows, fields) => {
-        if(!err) {
-            res.json(rows);
-        } else {
-            console.log(err);
-        }
-    });  
-});
-
-router.get('/cuenta/:id', (req, res) => {
-    const { id } = req.params;
-    mysqlConnection.query('select count(id) from files where idUser=?',[ id ], (err, rows, fields) => {
-        console.log(res)
-        if(!err) {
-            res.json(rows);
-        } else {
-            console.log(err);
-        }
-    });  
-});
->>>>>>> 2772ec34df411192a5976936a63c4a06e9930022
 module.exports=router;

793fd87900a884f5fefa2b92d03dc9c1d8eaa326
Expl0ced <56272035+Expl0ced@users.noreply.github.com>
2023-04-21 16:59:01 -0400

    Update recetas.js

--- src/routes/recetas/recetas.js
+++ src/routes/recetas/recetas.js
@@ -6,20 +6,26 @@ const cors = require('cors');
 
 const mysqlConnection = require('../../database.js');
 
+var corsOptions = {
+    origin: 'https://apihealthyfood.netlify.app',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 // GET all recetas
-router.get('/', cors(), (req, res) => {
+router.get('/', cors(corsOptions), (req, res) => {
     mysqlConnection.query('SELECT * FROM recetas', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
-            console.log(res)
         } else {
             console.log(err);
         }
     });
+
+    console.log('aaaaaaaaaa')
 });
 
 
-router.get('/:id', cors(), (req, res) => {
+router.get('/:id', cors(corsOptions), (req, res) => {
     const { id } = req.params;
     mysqlConnection.query('SELECT * FROM recetas WHERE IdReceta=?', [id], (err, rows, fields) => {
         if (!err) {
@@ -29,7 +35,7 @@ router.get('/:id', cors(), (req, res) => {
         }
     })
 });
-router.get('/ultima/ingreso', cors(), (req, res) => {
+router.get('/ultima/ingreso', cors(corsOptions), (req, res) => {
     mysqlConnection.query('select IdReceta from recetas ORDER BY IdReceta DESC LIMIT 1;', (err, rows, fields) => {
         console.log(req.body)
         if (!err) {
@@ -40,7 +46,7 @@ router.get('/ultima/ingreso', cors(), (req, res) => {
     })
 });
 
-router.post('/', cors(), (req, res) => {
+router.post('/', cors(corsOptions), (req, res) => {
     const { IdReceta, Encabezado, Ingredientes, Preparacion, Notas, Detalles, Imagen, Etiqueta } = req.body;
     const query = "CALL recetasADDorEDIT(?, ?, ?, ?, ?, ?, ? ,?)";
     mysqlConnection.query(query, [IdReceta, Encabezado, Ingredientes, Preparacion, Notas, Detalles, Imagen, Etiqueta], (err, rows, fields) => {
@@ -53,7 +59,7 @@ router.post('/', cors(), (req, res) => {
     });
 })
 
-router.put('/:id', cors(), (req, res) => {
+router.put('/:id', cors(corsOptions), (req, res) => {
     const { Encabezado, Ingredientes, Preparacion, Notas, Detalles, Imagen, Etiqueta } = req.body;
     const { Id } = req.params;
     // const query ="UPDATE employees SET name=?, salary=? WHERE id=?"
@@ -68,7 +74,7 @@ router.put('/:id', cors(), (req, res) => {
 });
 
 
-router.delete('/:id', cors(), (req, res) => {
+router.delete('/:id', cors(corsOptions), (req, res) => {
     const { Id } = req.params;
     mysqlConnection.query('DELETE FROM recetas WHERE id=?', [Id], (err, rows, fields) => {
         if (!err) {

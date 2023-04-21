1679e50fec8c8304244a2b9daca6c042297755d3
Expl0ced <56272035+Expl0ced@users.noreply.github.com>
2023-04-21 18:13:02 -0400

    a

--- src/routes/usuarios/usuarios.js
+++ src/routes/usuarios/usuarios.js
@@ -2,80 +2,80 @@
 
 const express = require('express');
 const router = express.Router();
-const sendMail= require('C:/Users/explo/APIRest tesis/src/mailer.js')
+const sendMail = require('C:/Users/explo/APIRest tesis/src/mailer.js')
 const cors = require('cors');
 
-const mysqlConnection  = require('../../database.js');
+const mysqlConnection = require('../../database.js');
 
 // GET all Employees
 router.get('/', cors(), (req, res) => {
-    mysqlConnection.query('SELECT Nombre, Apellido, Rol, Email FROM usuarios',(err, rows, fields) => {
-    if(!err) {
-        res.json(rows);
-    } else {
-        console.log(err);
-    }
-    });  
+    mysqlConnection.query('SELECT Nombre, Apellido, Rol, Email FROM usuarios', (err, rows, fields) => {
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
 });
 
 router.get('/usuarioExists/:email/:password', cors(), (req, res) => {
-    const { email, password}=req.params;
-    mysqlConnection.query('SELECT Email, Password FROM usuarios WHERE Email=? and Password=?', [email, password] ,(err, rows, fields) => {
-    if(!err) {
-        res.json(rows);
-    } else {
-        console.log(err);
-    }
-    });  
+    const { email, password } = req.params;
+    mysqlConnection.query('SELECT Email, Password FROM usuarios WHERE Email=? and Password=?', [email, password], (err, rows, fields) => {
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
 });
 
-router.get('/:id', cors(), (req, res)=>{
+router.get('/:id', cors(), (req, res) => {
     const { id } = req.params;
-    mysqlConnection.query('SELECT * FROM usuarios WHERE idUser=?', [id], (err, rows, fields)=>{
-        if(!err){
+    mysqlConnection.query('SELECT * FROM usuarios WHERE idUser=?', [id], (err, rows, fields) => {
+        if (!err) {
             res.json(rows[0]);
-        }else{
+        } else {
             console.log(err);
         }
     })
 });
 
-router.post('/', cors(),(req, res)=>{
-    const { idUser, Nombre, Apellido, Rol, Email, Password, Sintomas, Img, Peso, Altura, Genero, Contex_Fisica} = req.body;
+router.post('/', cors(), (req, res) => {
+    const { idUser, Nombre, Apellido, Rol, Email, Password, Sintomas, Img, Peso, Altura, Genero, Contex_Fisica } = req.body;
     const query = "CALL usuarioADDorEDIT(?,?,?,?,?,?,?,?,?,?,?,?)";
-    mysqlConnection.query(query, [idUser, Nombre, Apellido, Rol, Email, Password, Sintomas, Img, Peso, Altura, Genero, Contex_Fisica ], async (err, rows, fields)=>{
-        if(!err){
+    mysqlConnection.query(query, [idUser, Nombre, Apellido, Rol, Email, Password, Sintomas, Img, Peso, Altura, Genero, Contex_Fisica], async (err, rows, fields) => {
+        if (!err) {
             sendMail(Email)
-            res.json({Status:'Usuario Guardado'})
-        }else{
-            console.log(err)            
+            res.json({ Status: 'Usuario Guardado' })
+        } else {
+            console.log(err)
         }
     });
     // sendMail(Email)
 })
 
-router.put('/:id', cors(), (req, res)=>{
-    const { Nombre, Apellido, Rol, Email, Password, Sintomas, Img, Peso, Altura, Genero} = req.body;
-    const { id } = req.params;  
+router.put('/:id', cors(), (req, res) => {
+    const { Nombre, Apellido, Rol, Email, Password, Sintomas, Img, Peso, Altura, Genero } = req.body;
+    const { id } = req.params;
     const query = "CALL usuarioADDorEDIT(?,?,?,?,?,?,?,?,?,?,?)";
-    mysqlConnection.query(query, [id, Nombre, Apellido, Rol, Email, Password, Sintomas, Img, Peso, Altura, Genero ], (err, rows, fields)=>{
-        if(!err){
-            res.json({Status: 'Usuario Actualizado'});
-        }else{
+    mysqlConnection.query(query, [id, Nombre, Apellido, Rol, Email, Password, Sintomas, Img, Peso, Altura, Genero], (err, rows, fields) => {
+        if (!err) {
+            res.json({ Status: 'Usuario Actualizado' });
+        } else {
             console.log(err);
         }
     });
-}); 
+});
 
 
-router.delete('/:id', cors(), (req, res)=>{
-    const {id}= req.params;
-    mysqlConnection.query('DELETE FROM usuarios WHERE idUser=?', [id], (err, rows, fields)=>{
-        if(!err){
-            res.json({Status:'Usuario Borrado'});
+router.delete('/:id', cors(), (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('DELETE FROM usuarios WHERE idUser=?', [id], (err, rows, fields) => {
+        if (!err) {
+            res.json({ Status: 'Usuario Borrado' });
         }
     });
-});     
+});
 
 
-module.exports=router;  
+module.exports = router;  

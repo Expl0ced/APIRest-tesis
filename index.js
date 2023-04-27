67115706d0ff1c0d00cc30c563756d2d2ad1dbca
Expl0ced <56272035+Expl0ced@users.noreply.github.com>
2023-04-27 00:49:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.use(cors({
     methods: ["GET","HEAD","PUT","PATCH","POST","DELETE","OPTIONS"], // Permitir solicitudes con estos métodos HTTP
     allowedHeaders: ['Content-Type', 'Authorization'], // Permitir solicitudes con estos encabezados
 }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // Actualiza "*"" con el dominio de tu frontend
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+}); 
 app.use(express.json());
 app.use(bodyparser.urlencoded({
     extended: true

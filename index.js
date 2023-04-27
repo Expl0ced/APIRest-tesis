8bd76446c981ca1553a24cf423baf082a6edb012
Expl0ced <56272035+Expl0ced@users.noreply.github.com>
2023-04-27 02:45:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'], // Reemplaza con los encabezados que necesitas
     credentials: true // Habilita el envío de cookies de origen cruzado
 }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 

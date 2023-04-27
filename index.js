72bed148505d9827839b112aaa36cc17bbbd1751
Expl0ced <56272035+Expl0ced@users.noreply.github.com>
2023-04-27 00:42:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('port', process.env.PORT || 3000);
 // Middlewares
 app.use(cors({  
     origin: ['http://localhost:4200', 'https://healthyfoodpage.netlify.app'], // Permitir solicitudes desde estos dos orígenes
-    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"], // Permitir solicitudes con estos métodos HTTP
+    methods: ["GET","HEAD","PUT","PATCH","POST","DELETE","OPTIONS"], // Permitir solicitudes con estos métodos HTTP
     allowedHeaders: ['Content-Type', 'Authorization'], // Permitir solicitudes con estos encabezados
     preflightContinue: false,
 }));

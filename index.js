800fe7f5c1def6c005783257eb1592f3c9017dbc
Expl0ced <56272035+Expl0ced@users.noreply.github.com>
2023-04-27 01:25:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'], // Permitir solicitudes con estos encabezados
     optionsSuccessStatus: 200
 }));
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:4200/"); // Actualiza "*"" con el dominio de tu frontend
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header('Access-Control-Max-Age', 86400)
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:4200"); // Actualiza "*"" con el dominio de tu frontend
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Max-Age', 86400)
+    next();
+});
 
 
 app.use(express.json());

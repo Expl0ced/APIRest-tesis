b4248a642bc6a18d9717bfba2773ffc25ba20bd2
Expl0ced <56272035+Expl0ced@users.noreply.github.com>
2023-05-15 16:31:35 -0400

    Update usuarios.js

--- src/routes/usuarios/usuarios.js
+++ src/routes/usuarios/usuarios.js
@@ -85,20 +85,18 @@ router.delete('/:id', cors(), (req, res) => {
     });
 });
 
-router.put('/update-img/:id', (req, res) => {
+router.put('/update-img/:id', (req, res)=>{    
     const { imagen } = req.body;
     const { id } = req.params;
     console.log(imagen)
-    mysqlConnection.query(`update usuarios, orden_nutri 
-                            set usuarios.Img=orden.nutri.img, usuarios.Img=? 
-                            where usuarios.idUser = orden_nutri.idClienteNutri and usuarios.idUser`, [imagen, id], (err, rows, fields) => {
+    mysqlConnection.query("update usuarios, orden_nutri set usuarios.Img=?, usuarios.Img=orden.nutri.img where usuarios.idUser=? and usuarios.idUser = orden_nutri.idClienteNutri", [ imagen, id ], (err, rows, fields)=>{    
         console.log(req.body)
-        if (!err) {
-            res.json({ Status: 'Foto de Perfil Actualizada' });
-        } else {
+        if(!err){
+            res.json({Status: 'Foto de Perfil Actualizada'});
+        }else{
             console.log(err);
         }
     });
-});
+}); 
 
 module.exports = router;  

57d4907fc437083f480fb0964b00568a396c57ce
Expl0ced <56272035+Expl0ced@users.noreply.github.com>
2023-08-17 16:11:11 -0400

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2');
 
 const mysqlConnection = mysql.createConnection({
-    host: 'grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'y8xou81fpvhxueyg',
-    password: 'iddp8prwfss26k9u',
-    database: 'sij2vou7a0rrqrs9',
-    multipleStatements: true
-
-    // host: 'localhost',
-    // user: 'exploced',
-    // password: '',
-    // database: 'recetas',
+    // host: 'grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    // user: 'y8xou81fpvhxueyg',
+    // password: 'iddp8prwfss26k9u',
+    // database: 'sij2vou7a0rrqrs9',
     // multipleStatements: true
+
+    host: 'sql10.freemysqlhosting.net',
+    user: 'sql10640232',
+    password: 'i5lXVdmqrk',
+    database: 'sql10640232',
+    multipleStatements: true
 
 });
 
